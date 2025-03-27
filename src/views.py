a3bebcb1c2330da703b532964ed0ec266a8fa302
Elena_Syurtukova <evasineva@bk.ru>
2025-03-28 01:04:23 +0900

    main func

--- src/views.py
+++ src/views.py
@@ -24,32 +24,52 @@ def main_func(date: str, period: str = 'M'):
     df = func_read_file_excel('../data/operations.xlsx')  # вызываем функцию для получения данных из файла excel
     print(date1, date2)
     df_OK = df.loc[df['Статус'] == 'OK'] # фильтруем только успешные операции
-
+    print(df_OK)
     filtered_df = filter_by_period(date1, date2, df_OK) # вызываем функцию для фильтрации датафрейма по датам
     print(filtered_df)
+
+   # расходная часть
     expenses_df = filtered_df[filtered_df['Сумма операции'] < 0] # фильтруем только расходные операции
     total_amount_expenses = round(expenses_df['Сумма операции с округлением'].sum()) # определяем сумму расходных операций
     category_grouped_expenses_df = expenses_df.groupby('Категория')['Сумма операции с округлением'].sum()
     category_expenses_df = category_grouped_expenses_df.sort_values(ascending=False)
     category_expenses_dict = category_expenses_df.to_dict()
     print(category_expenses_dict)
+    print(type(category_expenses_dict))
+    print(len(category_expenses_dict))
+    if len(category_expenses_dict) <= 7:
+        category_expenses_list = []
+        for key, value in category_expenses_dict.items():
+            elem = {}
+            elem['category'] = key
+            elem['amount'] = round(value)
+            category_expenses_list.append(elem)
+        print(category_expenses_list)
+    elif len(category_expenses_dict) > 7:
+        category_expenses_list = [{'category': key, 'amount': round(value)} for key, value in category_expenses_dict.items()]
+        elem_7 = category_expenses_list[:7]
+        elem_last = category_expenses_list[7:]
+        sum_last = sum(elem['amount'] for elem in elem_last)
+        last_value = {'category': 'Остальное', 'amount': round(sum_last)}
+        category_expenses_list = elem_7 + [last_value]
+        print(category_expenses_list)
+
+    # определяем сумму операций c наличными и переводами
+    amount_cash = round(filtered_df[filtered_df['Категория'] == 'Наличные']['Сумма операции'].sum())
+    amount_transfers = round(filtered_df[filtered_df['Категория'] == 'Переводы']['Сумма операции'].sum())
+    list_cash_transfers = [{'category': 'Наличные', 'amount': amount_cash},
+                           {'category': 'Переводы', 'amount': amount_transfers}]
+    # доходная часть
+    incoming_df = filtered_df[filtered_df['Сумма операции'] > 0]  # фильтруем только доходные операции
+    total_amount_incoming = round(incoming_df['Сумма операции с округлением'].sum())  # определяем сумму доходных операций
+    category_grouped_incoming_df = incoming_df.groupby('Категория')['Сумма операции с округлением'].sum()
+    category_incoming_df = category_grouped_incoming_df.sort_values(ascending=False)
+    category_incoming_dict = category_incoming_df.to_dict()
+    category_incoming_list = [{'category': key, 'amount': round(value)} for key, value in
+                              category_incoming_dict.items()]
+    print(category_incoming_list)
 
 
-
-
-
-
-def expenses(df: list) -> list:
-    # sum = 0
-    expenses_trans = []
-    # for trans in df:
-    #     if float(trans['Сумма платежа']) < 0:
-    #         sum += float(trans['Сумма операции с округлением'])
-    #         trans['Сумма операции с округлением'] = float(trans['Сумма операции с округлением'])
-    #         expenses_trans.append(trans)
-    #         sum_of_expenses = round(sum) # сумма всех расходных операций
-
-    pass
 
 
 
